743091261011bf40e44a90b637a2014ea7922eff
thales henrique bruninx <113862443+thalesbruninx@users.noreply.github.com>
2022-11-23 18:55:42 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,10 @@
+describe("Suite de teste com NodeJS", function(){
+
+	it("Demonstrar uso do Jasmine com NodeJS", function(){
+		expect(true).toEqual(true);
+	});
+
+	it("deve demonstrar o uso de karma em modo de escuta", function(){
+		expect(1).toEqual(1);
+	});
+});
